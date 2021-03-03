200408cfe798aa65becefa29486aa43b794dfdd6
taiwoadebayo <taiwo@flutterwavego.com>
2021-03-02 17:36:42 -0800

    adjusted test value

--- app/src/main/java/com/datastructure/algorithm/Main.kt
+++ app/src/main/java/com/datastructure/algorithm/Main.kt
@@ -3,18 +3,8 @@ package com.datastructure.algorithm
 fun main() {
 //    BinaryGap().start()
     val links = Links()
-
-//    links.show()
-//    print("\n")
-
     links.insert(15)
-    links.insert(13)
-    links.insertAt(2, 1)
     links.insertAt(0, 3)
-//    links.insertAt(1, 4)
-//    links.insertAt(2, 5)
-//    links.insertAt(3, 7)
-//    links.insertAt(4, 2)
     links.deleteAt(1)
     links.show()
 }
