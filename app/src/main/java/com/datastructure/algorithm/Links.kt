c2ddbb5c7ce38f99337c1f57236d616a50420e6e
taiwoadebayo <taiwo@flutterwavego.com>
2021-03-02 17:32:23 -0800

    delete and deleteAt methods

--- app/src/main/java/com/datastructure/algorithm/Links.kt
+++ app/src/main/java/com/datastructure/algorithm/Links.kt
@@ -48,20 +48,6 @@ class Links {
 
     }
 
-    /**
-     * Get the head
-     * Traverse through the nodes and show data value
-     */
-
-    fun show() {
-
-        var currentHead = head
-
-        while (currentHead.hasData()) {
-            print(currentHead?.data.toString() + "->")
-            currentHead = currentHead?.next
-        }
-    }
 
     /**
      * set default node position to 1 and Get head
@@ -95,11 +81,86 @@ class Links {
                     currentHead = currentHead?.next
                     nodePosition++
                 }
-                if (currentHead == null) print("Cannot set node $position because node ${nodePosition - 1} is empty \n")
+                if (currentHead == null) println("Cannot set node $position because node ${nodePosition - 1} is empty \n")
             }
         } ?: kotlin.run {
-            if (position == 0) head = node else print("Cannot set node $position on an empty list")
+            if (position == 0) head =
+                node else println("Cannot set node $position on an empty list")
         }
 
+    }
+
+
+    /**
+     * Get the head
+     * Traverse the node and set the data of the last item to null to delete it
+     */
+
+    fun delete() {
+
+        var currentHead = head
+
+        head?.let {
+
+            while (currentHead.canTraverse()) currentHead = currentHead?.next
+            currentHead?.data = null
+
+        } ?: kotlin.run {
+            println("List is already empty")
+        }
+
+    }
+
+
+    /**
+     * set default node position to 1 and Get head
+     * if head is empty (no item, head is null) and position is 0, list is empty
+     * if head is not empty (item exists) and position is 0, set next item of the head to the head
+     * if head is not empty (item exists) and position is not 0, traverse the node with data, detect the position,
+     * set all items in next 2 node (currentHead.next.data) as the next node and set the currentHead to null
+     * Uses a detach and attach approach
+     */
+
+    fun deleteAt(position: Int) {
+
+        var nodePosition = 1
+        var currentHead = head
+
+        currentHead?.let {
+
+            if (position == 0) {
+                head = currentHead?.next
+            } else {
+                while (currentHead.hasData()) {
+                    if (nodePosition == position) {
+                        currentHead?.next?.data = null
+                        currentHead?.next = currentHead?.next?.next
+                        break
+                    }
+                    currentHead = currentHead?.next
+                    nodePosition++
+                }
+                if (currentHead == null) println("Cannot delete node $position because node ${nodePosition - 1} is empty \n")
+            }
+        } ?: kotlin.run {
+            if (position == 0) println("Cannot delete node $position on an empty list")
+        }
+
+    }
+
+
+    /**
+     * Get the head
+     * Traverse through the nodes and show data value
+     */
+
+    fun show() {
+
+        var currentHead = head
+
+        while (currentHead.hasData()) {
+            print(currentHead?.data.toString() + "->")
+            currentHead = currentHead?.next
+        }
     }
 }
